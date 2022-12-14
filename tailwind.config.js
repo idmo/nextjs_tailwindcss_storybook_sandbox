cff3596b7878d7738176895f033eb3a8fc133930
Brian Maggi <brianmaggi@gmail.com>
2022-12-14 13:30:27 -0800

    custom theme stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        danger: colors.red,
+        alert: colors.amber,
+        info: colors.neutral,
+        primary: colors.sky,
+        secondary: colors.zinc,
+      },
+    },
   },
   plugins: [],
 };
